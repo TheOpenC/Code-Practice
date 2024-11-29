4655ca309ad8887b4139f09805b54b510b0a681b
Drew Dudak <ddudak@gmail.com>
2024-11-28 23:23:34 -0500

    working on Color Code's 10 part series

---
+++ color-code/prototypal-inheritance/js/main.js
@@ -0,0 +1,11 @@
+class Person {
+    talk() {
+        return 'talking';
+    } 
+}
+const me = new Person();
+const you = new Person();
+
+//its useful to use constructors to add properties and then use inheritance for methods via the prototype. 
+
+
